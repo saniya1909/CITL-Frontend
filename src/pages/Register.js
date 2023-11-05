cdc9fe2c07a343f7852131d68162d8d7dc881e8b
Saniya Bangare <saniya.bangare@spit.ac.in>
2023-11-06 03:35:39 +0530

    First commit

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import "../styles/RegisterStyles.css";
-import RegisterRightLogo from "../images/register-right-logo.png";
+
 import { Link } from 'react-router-dom';
+import Login from '../images/login.jpg'
 
 const Register = () => {
   return (
     <div className='main-register-page'>
         <div className='register-form'>
             <div className='form-layout'>
+
                 <div className='headline'>Sign In to GetaWay</div>
+                <div className='tagline'>To travel to live</div>
+
                 <div className='fill-form'>
                     <form className='main-form'>
                         <input type='text' required='true' placeholder='username@gmail.com' />
                         <input type='text' required='true' placeholder='Password' />
-                        <button type='button'>Sign In</button>
+                        <button type='button'>Log In</button>
                     </form>
                 </div>
-                <Link to="/register">Register Here</Link>
+                <p className='register' style={{color:"green"}}>New here?<Link to="/register">Create an Account</Link></p>
+                
             </div>
         </div>
         <div className='register-right-image'>
-            <img src={RegisterRightLogo} alt='register-right-logo' />
+            <img src={Login} alt='register-right-logo'/>
         </div>
     </div>
   );
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import RegisterLogo from "../images/register-right-logo.png";
 import "../styles/RegisterPageStyles.css";
+import google from './images/google.png';
+import apple from './images/apple.png';
 
 const RegisterPage = () => {
   return (
@@ -11,16 +13,32 @@ const RegisterPage = () => {
         </div>
         <div className='register-right'>
             <div className='main-register-form'>
-                <div className='register-form-header'>Sign up for an Getaway</div>
+                <div className='register-form-header'>Sign up for a Getaway!</div>
+                <div className='tagline'>To travel to live</div>
+                <div className='google-apple'>
+                    <button type='button'><img className="google" alt="Google" src={google} style={{height:'12px'}}/>Sign up with Google</button>
+                    <button type='button'><img className="apple" alt="Apple" src={apple} style={{height:'12px'}}/>Sign up with Apple</button>
+                
+                </div>
+                <div className='line'>
+                    <hr style={{width:"13rem" }}></hr>
+                    <p style={{paddingInline:"0.5rem"}}>or with e-mail</p>
+                    <hr style={{width:"13rem" }}></hr>
+                </div>
+        
                 <div className='register-form'>
-                    <div className='name`'>
+                    <div className='name' style={{paddingRight:"2rem"}}>
                         <input placeholder='First name' type='text' required="true" />
                         <input placeholder='Last name' type='text' required="true" />
                     </div>
-                    <input placeholder='Last name' type='email' required="true" />
+                    <div className='email'>
+                    <input placeholder='username@email.com' type='email' required="true" />
                     <input placeholder='Password' type='password' required="true" />
-                    <button type='submit'>Sign Up</button>
+                    </div>
+                    
                 </div>
+                <p style={{textAlign:"center",padding:"1rem"}}>By creating an account, you are agreeing to our Privacy Policy and Electronics Communication Policy</p>  
+                <button className='submit' type='submit'>Sign Up</button>
             </div>
         </div>
     </div>
